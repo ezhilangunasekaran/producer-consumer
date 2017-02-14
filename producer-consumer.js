a5294c991c32f88367014c1bfd4960a95b961606
Ezhil <g.ezhilan1@gmail.com>
2017-02-14 12:21:13 -0600

    improved the code with better structuring

--- producer-consumer.js
+++ producer-consumer.js
@@ -1,40 +1,40 @@
-/**
- * Created by ezhilan on 2/13/17.
- */
-//Variable Declaration
+
+
 var B = []; // Buffer declaration
 var buff_limit = 24; // buffer limit is 24
 var S = buff_limit; // declaring space available with initial value set as buffer limit
 var I = 0; // declaring and initializing items available as 0
+var activity = document.getElementById("activity");
+var gui = document.getElementById("gui");
+var getProducerinput = document.getElementById("producer");
+var getConsumerinput = document.getElementById("consumer");
+var pInput,cInput;
 // click event for submit button.
 // getting values from inpur field
 document.getElementById("submit").addEventListener("click",function () {
-    var getProducerinput = document.getElementById("producer");
-    var getConsumerinput = document.getElementById("consumer");
-    window.P = parseInt(getProducerinput.value); // getting producer value
-    window.C = parseInt(getConsumerinput.value); // getting consumer value
+    pInput = parseInt(getProducerinput.value); // getting producer value
+    cInput = parseInt(getConsumerinput.value); // getting consumer value
     onclk();
 });
-var itr = "item";
+
 //Once started
 // function for Producer
 function producer() {
     var newProducerMessage = document.createElement("b");
-    if (S >= window.P) { // checking Space available is greater than or equal to producer
-        for (i = 0; i < window.P; i++) {
-            B.push(itr); // pushing items in to space in buffer.
+    if (S >= pInput) { // checking Space available is greater than or equal to producer
+        for (var i = 0; i < pInput; i++) {
+            B.push("item"); // pushing items in to space in buffer.
             var newPizza = document.createElement("li");
-            document.getElementById("gui").appendChild(newPizza);
-            //$("#gui").append("<li></li>");
+            gui.appendChild(newPizza);
         }
         // Notification for
-        console.log("Producer pushed" + window.P + "items");
+        console.log("Producer pushed" + pInput + "items");
 
-        newProducerMessage.textContent= "Producer pushed " + window.P + " items";
+        newProducerMessage.textContent= "Producer pushed " + pInput + " items";
         newProducerMessage.style = "color:green";
-        document.getElementById("activity").appendChild(newProducerMessage);
-        document.getElementById("activity").appendChild(document.createElement("br"));
-        //$("#activity").append("<b style=color:green>Producer pushed " + window.P + " items</b><br>");
+        activity.appendChild(newProducerMessage);
+        activity.appendChild(document.createElement("br"));
+
         console.log(B);
         I = B.length; // signaling no of items available
         console.log(I);
@@ -42,9 +42,9 @@ function producer() {
         console.log("no space available");
         newProducerMessage.textContent = "No space available";
         newProducerMessage.style = "color:red";
-        document.getElementById("activity").appendChild(newProducerMessage);
-        document.getElementById("activity").appendChild(document.createElement("br"));
-        //$("#activity").append("<b style=color:red>No space available<b><br>");
+        activity.appendChild(newProducerMessage);
+        activity.appendChild(document.createElement("br"));
+
     }
 }
 
@@ -52,20 +52,18 @@ function producer() {
 function consumer() {
     var newConsumerMessage = document.createElement("b");
     setTimeout(function() { // to maintain a 1sec gap between producer and consumer
-        if (I >= window.C) { // checking for available items
-            var gui = document.getElementById("gui");
-            for (i = 0; i < window.C; i++) {
-                B.splice(0, 1); // consuming items from the buffer
-                gui.removeChild(gui.childNodes[0]);
-                //$("li").first().remove();
-            }
-            console.log("Consumer removed " + window.C + " items");
+        if (I >= cInput) { // checking for available items
 
-            newConsumerMessage.textContent= "Consumer removed " + window.C + " items";
+            B.splice(0, cInput); // consuming items from the buffer
+            for (i = 0; i < cInput; i++) {
+                gui.removeChild(gui.lastChild);
+            }
+            console.log("Consumer removed " + cInput + " items");
+
+            newConsumerMessage.textContent= "Consumer removed " + cInput + " items";
             newConsumerMessage.style = "color:orange";
-            document.getElementById("activity").appendChild(newConsumerMessage);
-            document.getElementById("activity").appendChild(document.createElement("br"));
-            //$("#activity").append("<b style=color:orange>Consumer removed " + window.C + " items</b><br>");
+            activity.appendChild(newConsumerMessage);
+            activity.appendChild(document.createElement("br"));
             console.log(B);
             S = buff_limit - B.length; // signaling no of space available
             console.log(S);
@@ -73,15 +71,14 @@ function consumer() {
             console.log("no items available");
             newConsumerMessage.textContent = "No items available";
             newConsumerMessage.style = "color:red";
-            document.getElementById("activity").appendChild(newConsumerMessage);
-            document.getElementById("activity").appendChild(document.createElement("br"));
-            //$("#activity").append("<b style=color:red>No items available<b><br>");
+            activity.appendChild(newConsumerMessage);
+            activity.appendChild(document.createElement("br"));
         }
     }, 1000);
 }
 //click event triggered for submit button.
 function onclk() {
-    var set_prod_func = setInterval(producer, 2000); // initiating the producer function on click
-    var set_con_func = setInterval(consumer, 2000); // initiating the consumer function on click
+    setInterval(producer, 2000); // initiating the producer function on click
+    setInterval(consumer, 2000); // initiating the consumer function on click
     console.log("started");
 }
